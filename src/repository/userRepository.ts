6f164ee51f5fa46182c9ff8b02fe31632b2d7254
samssiq <sara.arianne.siqueira@ccc.ufcg.edu.br>
2025-07-14 09:14:42 -0300

    miniteste de banco de dados

---
+++ src/repository/userRepository.ts
@@ -0,0 +1,20 @@
+import { User } from "../models/user";
+
+
+export class UserRepository {
+    // Criar um novo usuário
+  async createUser(name: string, email: string, password: string) {
+    // Use o método `create` para salvar no banco de dados
+    return await User.create({
+      name,
+      email,
+      password
+    });
+  }
+
+
+  async getAllUsers() {
+    return await User.findAll();
+  }
+}
+
