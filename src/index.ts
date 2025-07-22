02b991c44d3576dbd251457c324f7c93dad90546
samssiq <sara.arianne.siqueira@ccc.ufcg.edu.br>
2025-07-22 10:34:11 -0300

    atualizações de rotas no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,56 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
-import  sequelize from "./config/database";
-import { UserRepository } from "./repository/userRepository";
+import sequelize from "./config/database";
 
+
+import { DisciplinaController } from "./controllers/disciplinaController";
+import { ContatoController } from "./controllers/contatoController";
+import { LembretesController } from "./controllers/lembretesController";
+import { MateriaisController } from "./controllers/materiaisController";
+import { UserController } from "./controllers/userController";
 
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
 
 
-const userRepo = new UserRepository();
+app.post("/user", async (req, res) => await UserController.create(req, res));
+app.get("/user", async (req, res) => await UserController.list(req, res));
+app.get("/user/:id", async (req, res) => await UserController.findById(req, res));
+app.patch("/user/:id", async (req, res) => await UserController.update(req, res));
+app.delete("/user/:id", async (req, res) => await UserController.delete(req, res));
+
+app.post("/disciplina", async (req, res) => await DisciplinaController.create(req, res));
+app.get("/disciplina/:id", async (req, res) => await DisciplinaController.findById(req, res));
+app.get("/disciplina", async (req, res) => await DisciplinaController.list(req, res));
+app.patch("/disciplina/:id", async (req, res) => await DisciplinaController.update(req, res));
+app.delete("/disciplina/:id", async (req, res) => await DisciplinaController.delete(req, res));
+
+app.post("/contatos", async (req, res) => await ContatoController.create(req, res));
+app.get("/contatos", async (req, res) => await ContatoController.list(req, res));
+app.get("/contatos/:id", async (req, res) => await ContatoController.findById(req, res));
+app.patch("/contatos/:id", async (req, res) => await ContatoController.update(req, res));
+app.delete("/contatos/:id", async (req, res) => await ContatoController.delete(req, res));
+
+app.post("/lembretes", async (req, res) => await LembretesController.create(req, res));
+app.get("/lembretes", async (req, res) => await LembretesController.list(req, res));
+app.patch("/lembretes/:id", async (req, res) => await LembretesController.update(req, res));
+app.delete("/lembretes/:id", async (req, res) => await LembretesController.delete(req, res));
+app.get("/lembretes/:id", async (req, res) => await LembretesController.findById(req, res));
+
+app.post("/materiais", async (req, res) => await MateriaisController.create(req, res));
+app.get("/materiais", async (req, res) => await MateriaisController.list(req, res));
+app.patch("/materiais/:id", async (req, res) => await MateriaisController.update(req, res));
+app.delete("/materiais/:id", async (req, res) => await MateriaisController.delete(req, res));
+app.get("/materiais/:id", async (req, res) => await MateriaisController.findById(req, res));
 
 
-app.post("/users", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const user = await userRepo.createUser(name, email, password);
-    res.json(user); // Retorna o usuário criado
-  } catch (error: any) {
-    res.status(500).json({ message: "Erro ao criar o usuário", error: error.message });
-  }
-});
-
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await userRepo.getAllUsers();
-    res.json(users); // Retorna todos os usuários
-  } catch (error: any) {
-    res.status(500).json({ message: "Erro ao obter os usuários", error: error.message });
-  }
-});
-
-
-// Testando a conexão e inicializando o servidor
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   console.log("Banco de dados conectado!");
   app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-}).catch((error: any) => {
+}).catch((error) => {
   console.error("Erro ao conectar ao banco de dados:", error);
 });
+
 
